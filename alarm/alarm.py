2c71149a5d8d22d003025585be78fa1e27e2bde7
yadukrishnakp <yadukrishnakp007@gmail.com>
2021-01-26 16:31:21 +0530

    alarm tone completed

--- alarm/alarm.py
+++ alarm/alarm.py
@@ -1,17 +1,16 @@
 import datetime
-
+from playsound import playsound
 
 
 alarmhour=int(input("enter the hour do you want to wakeup"))
 alarmminute=int(input("enter the minute do you want wakeup"))
-ampm=str(input("enter am or pm"))
-
+ampm=str(input("enter am or pm :"))
 if ampm=="pm":
     alarmhour=alarmhour+12
 
-while(1==1):
+while 1==1:
     if alarmhour==datetime.datetime.now().hour and alarmminute==datetime.datetime.now().minute:
         print("wakeup wakwup....")
-
+        playsound("zvonok_starogo_budilnika.mp3")
+        playsound("zvonok_starogo_budilnika.mp3")
         break
-
