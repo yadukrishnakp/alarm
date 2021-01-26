8332f70045b42c92f1b5f52655fa472f55e1a011
yadukrishnakp <yadukrishnakp007@gmail.com>
2021-01-26 16:34:53 +0530

    made a picture while alarm is turn on

--- alarm/alarm.py
+++ alarm/alarm.py
@@ -1,3 +1,4 @@
+from PIL import Image
 import datetime
 from playsound import playsound
 
@@ -11,6 +12,8 @@ if ampm=="pm":
 while 1==1:
     if alarmhour==datetime.datetime.now().hour and alarmminute==datetime.datetime.now().minute:
         print("wakeup wakwup....")
+        p = Image.open("cartoon-alarm-clock-ringing-vector-23354608.jpg")
+        p.show()
         playsound("zvonok_starogo_budilnika.mp3")
         playsound("zvonok_starogo_budilnika.mp3")
         snooze=(input("set snooze yes or no"))
