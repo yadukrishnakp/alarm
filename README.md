f13d37b5ae4d18226cc480cd1d777ed76692e682
yadukrishnakp <yadukrishnakp007@gmail.com>
2021-07-20 18:59:47 +0530

    README.md and requirements.txt files are edited

---
+++ README.md
@@ -0,0 +1,6 @@
+# Simple Alarm
+This is alarm program. In this user can set alarm in any time and this will provide alarm snooze also.
+# Installation 
+pip3 install -r requirements.txt
+# For Run
+python3 alarm.py
--- alarm/alarm.py
+++ alarm/alarm.py
@@ -3,23 +3,23 @@ import datetime
 from playsound import playsound
 
 
-alarmhour=int(input("enter the hour do you want to wakeup"))
-alarmminute=int(input("enter the minute do you want wakeup"))
-ampm=str(input("enter am or pm :"))
+alarmhour=int(input("enter the hour do you want to wakeup :-"))
+alarmminute=int(input("enter the minute do you want wakeup :"))
+ampm=str(input("enter am or pm :-"))
 if ampm=="pm":
     alarmhour=alarmhour+12
 
 while 1==1:
     if alarmhour==datetime.datetime.now().hour and alarmminute==datetime.datetime.now().minute:
-        print("wakeup wakwup....")
+        print("wake up wake up....")
         p = Image.open("cartoon-alarm-clock-ringing-vector-23354608.jpg")
         p.show()
         playsound("zvonok_starogo_budilnika.mp3")
         playsound("zvonok_starogo_budilnika.mp3")
-        snooze=(input("set snooze yes or no"))
+        snooze=(input("set snooze, yes or no :-"))
         while 1==1:
             if snooze=='yes':
-                tsnooze=int(input("enter mitute for snooze"))
+                tsnooze=int(input("enter minute for snooze"))
                 alarmminute+=tsnooze
                 break
             else:
